365ea837d29faf27abc348c95c0ddcff2ac1fa4a
Maja Kostacinska <maja.kkostacinska@gmail.com>
2022-04-29 21:39:41 +0100

    Initial Commit

---
+++ Shared/The_Sleep_SynopsisApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Sleep_SynopsisApp.swift
+//  Shared
+//
+//  Created by Maja Kostacinska on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_Sleep_SynopsisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
