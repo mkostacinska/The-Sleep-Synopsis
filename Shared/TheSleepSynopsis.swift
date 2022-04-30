83475ab03588a9c25225131171d098dc7f2de793
Maja Kostacinska <maja.kkostacinska@gmail.com>
2022-04-30 12:23:42 +0100

    Feat: Started working on the main card

---
+++ Shared/TheSleepSynopsis.swift
@@ -0,0 +1,16 @@
+//
+//  TheSleepSynopsis.swift
+//  The Sleep Synopsis
+//
+//  Created by Maja Kostacinska on 30/04/2022.
+//
+
+import Foundation
+
+public class TheSleepSynopsis {
+    
+    ///  Return the bedtime set by the user in the (HH, mm) format.
+    public static func getBedtime() -> (Int, Int) {
+        return (23, 30)
+    }
+}
